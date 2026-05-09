a6e54ce629f455472e5c469063fab21bef72b155
negirenu026-a11y <negirenu026@gmail.com>
2026-05-09 12:56:35 +0530

    smtp

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,8 +3,8 @@ const nodemailer = require("nodemailer");
 const sendEmail = async (options) => {
     const transporter = nodemailer.createTransport({
         host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
+        port: 587,
+        secure: false,        // false = STARTTLS (not SSL)
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
